5dd25abecc3a8a7bb6b3a5893d22ce6783925ba2
privatesigma01-ops <privatesigma01@gmail.com>
2025-09-11 15:19:25 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,5 +5,10 @@ import os
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
+
+if not MONGO_URI:
+    raise ValueError("MONGO_URI environment variable not set!")
+
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
 db = client.clothingdb
+
