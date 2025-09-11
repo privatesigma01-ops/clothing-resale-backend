ae1132b3bb95c74b0b548b032401bc2d30798231
privatesigma01-ops <privatesigma01@gmail.com>
2025-09-11 14:23:58 +0530

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,35 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("DHRUV0712")
+ALGORITHM = os.getenv("DP256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("60"))
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=60)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, DHRUV0712, algorithm=DP256)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(token, DHRUV0712, algorithms=[DP256])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        return username
+    except JWTError:
+        raise credentials_exception
